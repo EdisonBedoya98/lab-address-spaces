1dc529a0ebc0c6f2c213001b47c7c436677efb3a
edison.bedoyag <edison.bedoyag@udea.edu.co>
2019-08-12 12:43:13 -0500

    update

--- memory-user.c
+++ memory-user.c
@@ -2,23 +2,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include  <sys/types.h>
+#include  <unistd.h>
+
 int main(int argc, char *argv[])
 {
     printf("%s\n", argv[1]);
+
+    pid_t pid = getpid();
+    printf(" This is the PID: %d", pid);
+    
     clock_t cl = clock();
-    int valorReservar = (atoi(argv[1]) * 1e6);
+    int valorReservar = (atoi(argv[1]) * 1e12);
     int *A_heap = malloc(valorReservar);
     int count = valorReservar / 4;
+    
+    
+
     if (argc == 2)
     {
-
+        
         while (1)
         {
             for (int i = 0; i < count; i++)
             {
                 *(A_heap + i) = 0;
             }
-            printf("lleno vector");
+         
         }
     }
     else
